1349748413dde211f0427d229452f9b7ce1b0646
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2020-05-28 18:56:01 -0400

    created Services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,28 @@
-import React from "react"
-import Title from "./Title"
-import services from "../constants/services"
-const Services = () => {
-  return <h2>services component</h2>
-}
+import React from "react";
 
-export default Services
+import Title from "./Title";
+import services from "../constants/services";
+
+const Services = () => {
+  return (
+    <section class="section bg-grey">
+      <Title title="services" />
+      <div class="section-center services-center">
+        {services.map((service) => {
+          const { id, icon, title, text } = service;
+
+          return (
+            <article key={id} className="service">
+              {icon}
+              <h4>{title}</h4>
+              <div className="underline"></div>
+              <p>{text}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,51 +1,54 @@
-import React from "react"
+import React from "react";
 import {
   FaFacebookSquare,
   FaLinkedin,
   FaDribbbleSquare,
   FaBehanceSquare,
   FaTwitterSquare,
-} from "react-icons/fa"
+  FaGithub
+} from "react-icons/fa";
 
 const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.twitter.com"
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-    url: "https://www.twitter.com",
+    url: "https://www.linkedin.com"
   },
   {
     id: 3,
     icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "https://www.twitter.com",
+    url: "https://www.dribble.com"
   },
   {
     id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "https://www.twitter.com",
+    icon: <FaGithub className="social-icon"></FaGithub>,
+    url: "https://github.com/tjohnson1106"
   },
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-    url: "https://www.twitter.com",
-  },
-]
-const links = data.map(link => {
+    url: "https://www.twitter.com/bracketfactory"
+  }
+];
+const links = data.map((link) => {
   return (
     <li key={link.id}>
       <a href={link.url} className="social-link">
         {link.icon}
       </a>
     </li>
-  )
-})
+  );
+});
+
+// optional
 
 export default ({ styleClass }) => {
   return (
     <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
-  )
-}
+  );
+};
